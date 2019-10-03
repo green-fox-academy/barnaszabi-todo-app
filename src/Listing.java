195e5f9b874885fda25042fa714317f7aad89135
Barna Szabolcs <barnaszabi97@gmail.com>
2019-10-03 15:21:16 +0200

    Continued ToDo application

--- src/Listing.java
+++ src/Listing.java
@@ -2,13 +2,19 @@ import java.util.List;
 
 public class Listing {
 
-  public void ListAll(List<String> tasks){
+  public void ListAll(List<Task> tasks){
+    String textpart = "";
     for (int i = 0; i < tasks.size(); i++) {
-      System.out.println((i+1) + " - " + tasks.get(i));
+      if (tasks.get(i).done){
+        textpart = "[x] ";
+      }else {
+        textpart = "[ ] ";
+      }
+      System.out.println(tasks.get(i).index + " - " + textpart + tasks.get(i).description);
     }
   }
 
-  public void ListEmpty(List<String> tasks){
+  public void ListEmpty(List<Task> tasks){
     System.out.println("No todos for today! :)");
   }
 }
